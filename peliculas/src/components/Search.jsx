7eea7cec75f2d6c7186f9cb95a9a974d44634372
juanjosebl2 <juanjosebl2@gmail.com>
2022-02-22 23:42:49 +0100

    proyecto basico sobre peliculas en react

---
+++ peliculas/src/components/Search.jsx
@@ -0,0 +1,45 @@
+import styles from './Search.module.css';
+import { FaSearch } from 'react-icons/fa';
+import { useEffect, useState } from "react";
+import {useNavigate} from 'react-router-dom';
+import { useQuery } from "../hooks/useQuery";
+
+
+export function Search() {
+    const query = useQuery();
+    const search = query.get("search");
+
+    const [searchText, setSearchText] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        /*if(search){
+            setSearchText(search);
+        } else {
+            setSearchText("");
+        }*/
+        setSearchText(search || "");
+    }, [search]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate("/?search=" + searchText);
+    }
+
+    return (
+        <form className={styles.searchContainer} onSubmit={handleSubmit}>
+            <div className={styles.searchBox}>
+                <input 
+                    className={styles.searchInput} 
+                    type="text" 
+                    value={searchText} 
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+
+                <button className={styles.searchButton} type="submit">
+                    <FaSearch size={20}/> 
+                </button>
+            </div>
+        </form>
+    )
+}
