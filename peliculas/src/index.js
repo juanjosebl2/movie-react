cd276c4a33c312ae55c2c9529a6702ddfcbe20ed
juanjosebl2 <juanjosebl2@gmail.com>
2023-08-06 11:12:44 +0200

    added good files

---
+++ peliculas/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { App } from './App';
+
+ReactDOM.render(
+
+  <App />,
+document.getElementById('root')
+);
+
+/*const contenido = <div>
+  <h1> Titulo </h1>
+  <div> Hola </div>
+</div>;
+
+function Componente(props){
+  console.log(props);
+  //const titulo = props.titulo;
+  const { titulo, contenido } = props;
+  return (
+    <div>
+      <h1> {titulo} </h1>
+      <div> {contenido} </div>
+    </div>
+  );
+}
+
+
+function Componente({titulo, children}){
+ 
+  return (
+    <div className='contenedor' >
+      <h1> {titulo} </h1>
+      <div> {children} </div>
+    </div>
+  );
+}*/
+
+
+
+
+
